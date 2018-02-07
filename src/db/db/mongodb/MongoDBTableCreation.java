349f7cf99162a2df2cfd5d66e53462bd0c8a822d
Not4Zy <519419490@qq.com>
2018-02-07 00:04:27 -0500

    Update

--- src/db/db/mongodb/MongoDBTableCreation.java
+++ src/db/db/mongodb/MongoDBTableCreation.java
@@ -2,6 +2,8 @@ package db.db.mongodb;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.IndexOptions;
+import org.bson.Document;
 
 /**
  * project: TicketProber
@@ -13,6 +15,21 @@ public class MongoDBTableCreation {
     public static void main(String[] args) {
         MongoClient mongoClient = new MongoClient();
         MongoDatabase db = mongoClient.getDatabase(MongoDBUtility.DB_NAME);
+
+        // drop old collection
+        db.getCollection("users").drop();
+        db.getCollection("items").drop();
+
+        // create new collection, insert fake user
+        db.getCollection("users").insertOne(new Document()
+                .append("user_id", "1111")
+                .append("password", "3229c1097c00d497a0fd282d586be050")
+                .append("first_name", "John")
+                .append("last_name", "Smith"));
+
+        IndexOptions indexOptions = new IndexOptions().unique(true);
+        db.getCollection("users").createIndex(new Document("user_id", 1), indexOptions);
+        db.getCollection("items").createIndex(new Document("item_id", 1), indexOptions);
 
         mongoClient.close();
         System.out.println("Import is done successfully.");
