b7180717bc8574fac411735c27295830b6fc76b9
Not4Zy <519419490@qq.com>
2018-02-18 17:28:41 -0500

    Update

---
+++ src/test/rpc/UtilityTest.java
@@ -0,0 +1,55 @@
+package test.rpc; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Utility Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Feb 18, 2018</pre> 
+* @version 1.0 
+*/ 
+public class UtilityTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: writeJsonObj(HttpServletResponse response, JSONObject obj) 
+* 
+*/ 
+@Test
+public void testWriteJsonObj() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: writeJsonArray(HttpServletResponse response, JSONArray array) 
+* 
+*/ 
+@Test
+public void testWriteJsonArray() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: readJsonObject(HttpServletRequest request) 
+* 
+*/ 
+@Test
+public void testReadJsonObject() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
